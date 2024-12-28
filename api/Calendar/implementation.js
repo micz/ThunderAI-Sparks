6415c06cd89c8a6022682555183be4727a05bc2d
mic <m@micz.it>
2024-12-28 21:05:08 +0100

    working on the API implementation

--- api/Calendar/implementation.js
+++ api/Calendar/implementation.js
@@ -1,9 +1,23 @@
-var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+// Using a closure to not leak anything but the API to the outside world.
+(function (exports) {
 
-var Calendar = class extends ExtensionCommon.ExtensionAPI {
-  getAPI(context) {
-    return {
-      Calendar: {}
+  var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+
+  var Calendar = class extends ExtensionCommon.ExtensionAPI {
+    getAPI(context) {
+      return {
+        Calendar: {
+          async openCalendarDialog(cal_data) {
+            // implementation
+          }
+        }
+      }
     }
-  }
-};
+  };
+
+
+  // Export the API by assigning it to the exports parameter of the anonymous
+  // closure function, which is the global this.
+  exports.Calendar = Calendar;
+
+})(this)
