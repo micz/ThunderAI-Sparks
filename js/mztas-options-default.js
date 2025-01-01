3d696c2f6f88551c1873e6ce2442e3d52ec06ae0
mic <m@micz.it>
2025-01-01 01:46:53 +0100

    options page and related libs added

---
+++ js/mztas-options-default.js
@@ -0,0 +1,3 @@
+export const prefs_default = {
+    'do_debug': false,
+}
---
+++ options/mztas-options-page.js
@@ -0,0 +1,35 @@
+/*
+ *  ThunderAI Sparks [https://micz.it/thunderbird-addon-thunderai/#sparks]
+ *  Copyright (C) 2024  Mic (m@micz.it)
+
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { tasLogger } from "../js/mztas-logger.js";
+import { i18n } from "../js/mztas-i18n.js";
+import { tasPrefs } from "../js/mztas-options.js";
+
+document.addEventListener('DOMContentLoaded', async () => {
+    let do_debug = await tasPrefs.getPref("do_debug");
+    tasPrefs.logger = new tasLogger("mztas-options", do_debug);
+
+    let destfoldertype_select = document.getElementById("dest_folder_type");
+    destfoldertype_select.addEventListener("change", showDestFolderOptions);
+
+    tasPrefs.restoreOptions();
+    i18n.updateDocument();
+    document.querySelectorAll(".option-input").forEach(element => {
+      element.addEventListener("change", tasPrefs.saveOptions);
+    });
+  }, { once: true });
