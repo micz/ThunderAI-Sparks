1b879e24e97ce83f3dbc515b8fbbe0924729da8c
mic <m@micz.it>
2025-03-27 23:01:59 +0100

    Remove commented-out error handling for disabled calendars in openCalendarDialog

--- api/CalendarTools/implementation.js
+++ api/CalendarTools/implementation.js
@@ -82,10 +82,10 @@
               console.log(">>>>>>>>>> ThunderAI Sparks: openCalendarDialog curr_calendar.name: ", JSON.stringify(curr_calendar.name));
               console.log(">>>>>>>>>> ThunderAI Sparks: openCalendarDialog curr_calendar.getProperty(\"disabled\"): ", JSON.stringify(curr_calendar.getProperty("disabled")));
 
-              if(curr_calendar.getProperty("disabled")) {
-                console.error("[ThunderAI Sparks] openCalendarDialog ExtensionAPI error: ", "No active calendar found!");
-                return {result: false, error: "|>>noActiveCalendar"};
-              }
+              // if(curr_calendar.getProperty("disabled")) {
+              //   console.error("[ThunderAI Sparks] openCalendarDialog ExtensionAPI error: ", "No active calendar found!");
+              //   return {result: false, error: "|>>noActiveCalendar"};
+              // }
 
               window.createEventWithDialog(
                 curr_calendar, //calendars[0], //cal_data.calendar,
